ef242bcac572dbf44feb8dc28e3912b066d88596
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2023-11-21 06:19:46 +0100

    Move `data` types into separate modules

---
+++ src/data/component.rs
@@ -0,0 +1,50 @@
+// SPDX-FileCopyrightText: OpenTalk GmbH <mail@opentalk.eu>
+// SPDX-License-Identifier: EUPL-1.2
+
+use std::collections::BTreeMap;
+
+use semver::Version;
+use serde::{Deserialize, Serialize};
+
+use super::{ComponentName, ComponentRelease};
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Component {
+    pub name: ComponentName,
+    pub gitlab_url: String,
+
+    #[serde(default)]
+    pub releases: BTreeMap<Version, ComponentRelease>,
+}
+
+impl Component {
+    pub fn get_releases(
+        &self,
+        after: Option<Version>,
+        until: Version,
+    ) -> BTreeMap<Version, ComponentRelease> {
+        if let Some(after) = after {
+            self.releases
+                .iter()
+                .filter_map(|(v, r)| {
+                    if *v > after && *v <= until {
+                        Some((v.clone(), r.clone()))
+                    } else {
+                        None
+                    }
+                })
+                .collect::<BTreeMap<Version, ComponentRelease>>()
+        } else {
+            self.releases
+                .iter()
+                .filter_map(|(v, r)| {
+                    if *v <= until {
+                        Some((v.clone(), r.clone()))
+                    } else {
+                        None
+                    }
+                })
+                .collect::<BTreeMap<Version, ComponentRelease>>()
+        }
+    }
+}
---
+++ src/data/release_series.rs
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: OpenTalk GmbH <mail@opentalk.eu>
+// SPDX-License-Identifier: EUPL-1.2
+
+use std::collections::BTreeMap;
+
+use semver::Version;
+use serde::{Deserialize, Serialize};
+use time::Date;
+
+use super::{Release, SeriesCodename};
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct ReleaseSeries {
+    pub codename: SeriesCodename,
+    pub end_of_life: Date,
+    pub releases: BTreeMap<Version, Release>,
+}
