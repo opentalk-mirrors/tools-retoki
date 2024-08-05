745d9b6a839d2f92c29ae7c577d29707f700ed0c
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2024-08-05 06:54:36 +0200

    chore: add project-wide deny rules

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+//! `relbo` is a command-line tool to be used as a release helper bot in CI
+
+#![deny(
+    bad_style,
+    dead_code,
+    improper_ctypes,
+    missing_debug_implementations,
+    missing_docs,
+    no_mangle_generic_items,
+    non_shorthand_field_patterns,
+    overflowing_literals,
+    path_statements,
+    patterns_in_fns_without_body,
+    private_bounds,
+    private_interfaces,
+    trivial_casts,
+    trivial_numeric_casts,
+    unconditional_recursion,
+    unnameable_types,
+    unused,
+    unused_allocation,
+    unused_comparisons,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_parens,
+    unused_qualifications,
+    unused_results,
+    while_true
+)]
+
 fn main() {
     println!("Hello, world!");
 }
