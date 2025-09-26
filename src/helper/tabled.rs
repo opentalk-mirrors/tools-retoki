67e775ad63e1a5d6b0e307640fe81f547159fc25
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2025-09-26 07:58:55 +0200

    feat: add `series show` subcommand
    This allows to get detailed information about a series when generating
    for the documentation page.

--- src/helper/tabled.rs
+++ src/helper/tabled.rs
@@ -1,6 +1,25 @@
 // SPDX-FileCopyrightText: OpenTalk GmbH <mail@opentalk.eu>
 // SPDX-License-Identifier: EUPL-1.2
 
+use indexmap::IndexMap;
+
+use crate::data::{ComponentIdentifier, ComponentVersion};
+
 pub(crate) fn display_option<T: ToString>(value: &Option<T>) -> String {
     value.as_ref().map(ToString::to_string).unwrap_or_default()
+}
+
+pub(crate) fn display_components(
+    components: &IndexMap<ComponentIdentifier, ComponentVersion>,
+) -> String {
+    components
+        .iter()
+        .map(|(identifier, version)| format!("{identifier}: {version}"))
+        .fold(String::new(), |a, b| {
+            if a.is_empty() {
+                b
+            } else {
+                a + ", " + b.as_str()
+            }
+        })
 }
