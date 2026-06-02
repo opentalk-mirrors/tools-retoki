5bd4e170e28f99124e984ab95ef49ed85210a331
Albrecht Weiche <a.weiche@opentalk.eu>
2026-06-02 14:29:43 +0200

    chore: relax linting rules and add clippy lints
    Warn is enough for the rustc lints. Only the allowed lints are raised to
    warn level.

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,15 @@
 
 //! `relbo` is a command-line tool to be used as a release helper bot in CI
 
-#![deny(
-    bad_style,
-    dead_code,
-    improper_ctypes,
+#![warn(
     missing_debug_implementations,
     missing_docs,
-    no_mangle_generic_items,
-    non_shorthand_field_patterns,
-    overflowing_literals,
-    path_statements,
-    patterns_in_fns_without_body,
-    private_bounds,
-    private_interfaces,
     trivial_casts,
     trivial_numeric_casts,
-    unconditional_recursion,
     unnameable_types,
-    unused,
-    unused_allocation,
-    unused_comparisons,
     unused_extern_crates,
-    unused_import_braces,
-    unused_parens,
     unused_qualifications,
-    unused_results,
-    while_true
+    unused_results
 )]
 
 use clap::Parser;
